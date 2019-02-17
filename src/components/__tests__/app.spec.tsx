dda32b12861435644931f9a3cbcfa45ea2f05bd2
Patricio Beltran <pbelsal@gmail.com>
2019-02-16 18:49:50 -0800

    fix tests

--- src/components/__tests__/app.spec.tsx
+++ src/components/__tests__/app.spec.tsx
@@ -4,6 +4,6 @@ import { shallow } from "enzyme";
 import { App } from "../app";
 
 it("renders the heading", () => {
-    const result = shallow(<App />).contains(<h1>Hello from the app!</h1>);
+    const result = shallow(<App />).contains(<h1>welcome</h1>);
     expect(result).toBeTruthy();
 });
